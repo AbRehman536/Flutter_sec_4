8abaa343befb382c4691a8c35b1bce160b4996b4
Abdullah <abdullahrehman1019@gmail.com>
2025-11-24 11:01:47 -0600

    Tab Bar done

--- lib/tab_Bar.dart
+++ lib/tab_Bar.dart
@@ -1,44 +1,64 @@
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
 
-class TabBarDemo extends StatefulWidget {
+class TabBarDemo extends StatelessWidget {
   const TabBarDemo({super.key});
 
-  @override
-  State<TabBarDemo> createState() => _TabBarDemoState();
-}
-
-class _TabBarDemoState extends State<TabBarDemo> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.yellow,
-          leading: Icon(Icons.arrow_back),
-          title: Text("Tab Bar Demo"),
-          bottom: TabBar(
-              labelColor: Colors.blue,
-              unselectedLabelColor: Colors.red,
-              indicatorColor: Colors.black,
-              tabs: [
-            Tab(icon: Icon(Icons.home), text: "Home",),
-            Tab(icon: Icon(Icons.person), text: "Profile",),
-            Tab(icon: Icon(Icons.settings), text: "Settings",),
-          ]),
+          title: Text("Tab Bar"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(70),
+            child: Container(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TabBar(
+                  labelColor: Colors.white,
+                    unselectedLabelColor: Colors.grey,
+                    indicatorColor: Colors.red,
+                    indicator: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    tabs: [
+                      Tab(icon: Icon(Icons.home),text: "Home",),
+                      Tab(icon: Icon(Icons.settings),text: "Setting",),
+                      Tab(icon: Icon(Icons.person),text: "Profile",),
+                    ]),
+              ),
+            ),
+          ),
         ),
         body: TabBarView(children: [
           Center(child: Column(children: [
-            Image.asset("assets/images/1348431.png"),
-            Text("Home")
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.home),
+                  hint: Text("Home")
+                ),
+              ),
+            )
           ],),),
           Center(child: Column(children: [
-            Icon(Icons.person),
-            Text("Fahad")
+            Image.asset("assets/images/Frame.png"),
+            Text("Settings"),
           ],),),
           Center(child: Column(children: [
-            Text("Settings")
-          ],),)
+            Icon(Icons.person, size: 50,),
+            Text("Abdullah Rehman")
+          ],),),
         ]),
       ),
     );
